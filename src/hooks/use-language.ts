e3633162b2065df7325dac94d8da8e80e349822d
mano <manoa@livenexx.net>
2025-09-03 14:49:44 +0300

    Config

---
+++ src/hooks/use-language.ts
@@ -0,0 +1,22 @@
+import type { TranslationKey } from "@/lib/translations";
+import { createContext, useContext } from "react";
+
+export type Language = "en" | "fr";
+
+export interface LanguageContextType {
+  language: Language;
+  toggleLanguage: () => void;
+  t: (key: TranslationKey) => string;
+}
+
+export const LanguageContext = createContext<LanguageContextType | undefined>(
+  undefined
+);
+
+export function useLanguage() {
+  const context = useContext(LanguageContext);
+  if (context === undefined) {
+    throw new Error("useLanguage must be used within a LanguageProvider");
+  }
+  return context;
+}
